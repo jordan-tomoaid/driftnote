778f324b4c09a19d4cb9035c0155d37353bb04df
jordan-tomoaid <jordan@tomoaid.com>
2026-06-30 16:13:53 +0800

    fix: match Reddit pixel loader src to current snippet (pixel_id param)

--- src/components/site/PixelScripts.tsx
+++ src/components/site/PixelScripts.tsx
@@ -18,7 +18,7 @@ export function PixelScripts() {
     <>
       {REDDIT_PIXEL_ID && (
         <Script id="reddit-pixel" strategy="afterInteractive">
-          {`!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','${REDDIT_PIXEL_ID}');rdt('track','PageVisit');`}
+          {`!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=${REDDIT_PIXEL_ID}",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','${REDDIT_PIXEL_ID}');rdt('track','PageVisit');`}
         </Script>
       )}
 
